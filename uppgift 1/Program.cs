8531ef598c89b212089cdd8ca45ecb3041315882
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-09-12 14:28:46 +0200

    Add project files.

---
+++ uppgift 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är snart klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva ut tecken \" och \\");
+
+        }
+    }
+}
+
+
